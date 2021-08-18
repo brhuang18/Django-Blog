2c7393623cbd4cc1bc997a79ae9f5d4146cb23af
Brian Huang <brhuang18@gmail.com>
2021-08-18 23:40:58 +1000

    working home and about page with navbar

---
+++ django_project/blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'brhuang18',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 18, 2021'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 18, 2021'
+    }
+]
+# Create your views here.
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+    
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
