3c2be3016e19de955429bcb8d26ed3bc9fe87563
Brian Huang <brhuang18@gmail.com>
2021-08-19 00:41:58 +1000

    added db

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'brhuang18',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 18, 2021'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 18, 2021'
-    }
-]
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
     
